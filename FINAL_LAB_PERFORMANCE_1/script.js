e7e544ab776e250e8d1c44b6e51b0bc0e8e95fad
Nooren Zabran Ami <nooren.omi@gmail.com>
2021-11-22 16:48:33 +0600

    Lab

--- FINAL_LAB_PERFORMANCE_1/script.js
+++ FINAL_LAB_PERFORMANCE_1/script.js
@@ -1,6 +1,24 @@
-function btn(key) {
-    let resultElement = document.getElementById("result");
-    console.log(key);
-    if (!isNaN(key)) {
-      let res = resultElement.innerHTML;
-      resultElement.innerHTML = res == 0 ? key : res + key;
+var operator = document.getElementsByClassName("operator");
+for(var i =0;i<operator.length;i++){
+		if(this.id=="clear"){
+			printHistory("");
+			printOutput("");
+		}
+        else if(this.id=="backspace"){
+			var output=reverseNumberFormat(getOutput()).toString();
+			if(output){
+				output= output.substr(0,output.length-1);
+				printOutput(output);
+			}
+
+
+var number = document.getElementsByClassName("number");
+    for(var i =0;i<number.length;i++){
+        number[i].addEventListener('click',function(){
+        var output=reverseNumberFormat(getOutput());
+            if(output!=NaN){ 
+                output=output+this.id;
+                printOutput(output);
+                }
+            });
+        }
