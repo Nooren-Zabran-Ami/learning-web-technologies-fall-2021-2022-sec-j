9dc8c94d8fc228b7046e9bee80fcbc048d79b334
Nooren Zabran Ami <nooren.omi@gmail.com>
2021-11-24 15:45:07 +0600

    LAB-2

--- FINAL_LAB_PERFORMANCE_2/server.php
+++ FINAL_LAB_PERFORMANCE_2/server.php
@@ -8,4 +8,13 @@ if(isset($_POST)){
             if(strlen($old)>0) {
                 $last = substr($old, -1);
             }
-           
+            if($old <> '' and !in_array($last, $operators)){
+                $out = eval("return ($old);");
+             } else {
+                 $out = $old;
+             }
+         break;
+         default: $out = $old;
+    }
+    echo $out;
+} 
